0e8b78662f7268455e2bfb5e9a37865563a3c898
Dastan <dastan-4@mail.ru>
2021-02-11 21:09:00 +0600

    hw 14

--- app/src/main/java/com/example/myapplication/BattaryBroadcastReceiver.java
+++ app/src/main/java/com/example/myapplication/BattaryBroadcastReceiver.java
@@ -6,7 +6,12 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.BatteryManager;
+import android.os.PowerManager;
+import android.provider.Settings;
 import android.widget.Toast;
 
 import androidx.core.app.NotificationCompat;
@@ -19,6 +24,13 @@ public class BattaryBroadcastReceiver extends BroadcastReceiver {
         int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
         int batteryLevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
         boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||  status == BatteryManager.BATTERY_STATUS_FULL;
+        SharedPreferences sharedPref = context.getSharedPreferences("battery",Context.MODE_PRIVATE);
+
+        Boolean sound = sharedPref.getBoolean("sound", false);
+        Boolean vibration = sharedPref.getBoolean("vibration", false);
+        Boolean dialogSwitch = sharedPref.getBoolean("dialogSwitch", false);
+
+
         String text = "";
         if(isCharging && batteryLevel < 100){
             text = "Питание подключено - " + batteryLevel + "%";
@@ -34,15 +46,16 @@ public class BattaryBroadcastReceiver extends BroadcastReceiver {
 
         if(!isCharging && batteryLevel < 15){
             text = "Почти разряжена  - " + batteryLevel + "%";
-
-
-            AlertDialog.Builder builder = new AlertDialog.Builder(context);
-            builder.setMessage("Телефон разряжан")
-                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                        }
-                    });
-            builder.create().show();
+            Settings.System.putInt(context.getContentResolver(),Settings.System.SCREEN_BRIGHTNESS,10);
+            if(dialogSwitch) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setMessage("Телефон разряжан")
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                            }
+                        });
+                builder.create().show();
+            }
         }
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "Battery")
@@ -51,7 +64,13 @@ public class BattaryBroadcastReceiver extends BroadcastReceiver {
                 .setContentText(text.toString())
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-
+        if(sound){
+            Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            builder.setSound(uri);
+        }
+        if (vibration){
+            builder.setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 });
+        }
         notificationManager.notify(1, builder.build());
 
 //        Toast.makeText(context,batteryLevel,Toast.LENGTH_LONG).show();
--- app/src/main/java/com/example/myapplication/BroadcastReceiverActivity.java
+++ app/src/main/java/com/example/myapplication/BroadcastReceiverActivity.java
@@ -7,10 +7,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.BatteryManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,6 +21,13 @@ public class BroadcastReceiverActivity extends AppCompatActivity {
 
     Button start;
     Button stop;
+
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    Switch sound;
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    Switch vibration;
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    Switch dialogSwitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +35,37 @@ public class BroadcastReceiverActivity extends AppCompatActivity {
 
         start = findViewById(R.id.startCheck);
         stop = findViewById(R.id.stopCheck);
+        sound = findViewById(R.id.sound);
+        vibration = findViewById(R.id.vibration);
+        dialogSwitch = findViewById(R.id.dialogSwitch);
+
+        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("battery",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        sound.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                editor.putBoolean("sound", isChecked);
+                editor.apply();
+            }
+        });
+
+        vibration.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                editor.putBoolean("vibration", isChecked);
+                editor.apply();
+            }
+        });
+
+        dialogSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                editor.putBoolean("dialogSwitch", isChecked);
+                editor.apply();
+            }
+        });
+
         BroadcastReceiver broadcastReceiver = new BattaryBroadcastReceiver();
 
         start.setOnClickListener(new View.OnClickListener() {
@@ -41,4 +82,6 @@ public class BroadcastReceiverActivity extends AppCompatActivity {
             }
         });
     }
+
+//    public int getBooleanToInt
 }
